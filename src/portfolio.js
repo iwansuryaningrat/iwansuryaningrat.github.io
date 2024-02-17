55d010b8398dbd77ee3db605d8c59e213568352a
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2024-02-17 22:13:12 +0700

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,12 +23,12 @@ const greeting = {
 const socialMediaLinks = {
   /* Your Social Media Link */
   github: "https://github.com/iwansuryaningrat",
-  linkedin: "https://www.linkedin.com/in/harikrushn-kanani/",
+  linkedin: "https://www.linkedin.com/in/iwan-suryaningrat/",
   gmail: "iwan.suryaningrat28@gmail.com",
-  // gitlab: "https://gitlab.com/harikanani",
-  // facebook: "https://www.facebook.com/harikrishan.kanani/",
-  twitter: "https://twitter.com/Harikrushn9",
-  instagram: "https://www.instagram.com/hari_kanani_/",
+  // gitlab: "https://gitlab.com/",
+  // facebook: "https://www.facebook.com/",
+  twitter: "https://twitter.com/sningrat_",
+  instagram: "https://www.instagram.com/sningrat_/",
 };
 
 const skills = {
@@ -549,7 +549,7 @@ const projectsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "harikrushn.jpg",
+    profile_image_path: "iwansuryaningrat.jpg",
     description:
       "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
   },
@@ -557,7 +557,7 @@ const contactPageData = {
     title: "Blogs",
     subtitle:
       "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
-    link: "https://twitter.com/Harikrushn9",
+    link: "https://twitter.com/sningrat_",
     avatar_image_path: "blogs_image.svg",
   },
 };
