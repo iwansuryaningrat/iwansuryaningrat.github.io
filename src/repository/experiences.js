8be07c056a89d46492002b3999a4934875a012a3
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2024-03-08 04:53:25 +0700

    Fix English

--- src/repository/experiences.js
+++ src/repository/experiences.js
@@ -1,5 +1,5 @@
 const experience = {
-  title: "Experience",
+  title: "Experiences",
   subtitle: "Work, Internship and Volunteership",
   description:
     "I've completed one internship. I've mostly done projects on my own and I am actively looking for internships. I love organizing workshops to share my knowledge with others.",
