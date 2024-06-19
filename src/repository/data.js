09f7695b97f1caf3d72749c48cb16702a251bd33
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2024-06-19 14:26:56 +0700

    change resume link

--- src/repository/data.js
+++ src/repository/data.js
@@ -13,7 +13,7 @@ const greeting = {
   subTitle: "Backend Developer, NodeJs Specialist 🔥 Freelancer 💻",
   subTitle2: "Always learning.",
   resumeLink:
-    "https://drive.google.com/file/d/1dZiaor7hmeuG-lKmpBivbSJLVLyiJQu0/view?usp=sharing",
+    "https://drive.google.com/file/d/19hCc2eFR-PtpbQPm9GxQhuYgUO9ymcyY/view?usp=sharing",
   mail: "mailto:iwan.suryaningrat28@gmail.com",
 };
 
