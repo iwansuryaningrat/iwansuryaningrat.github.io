939061bcdfb3d7c4505c54bcbce5a056829b401c
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2024-08-05 21:22:05 +0700

    Fix Navbar Route

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -97,7 +97,7 @@ function Header(props) {
                 activeStyle={{ fontWeight: "bold" }}
                 style={{ borderRadius: 5, color: theme.text }}
               >
-                Education and Certifications
+                Educations
               </NavLink>
             </li>
             <li>
@@ -130,7 +130,7 @@ function Header(props) {
                 activeStyle={{ fontWeight: "bold" }}
                 style={{ borderRadius: 5, color: theme.text }}
               >
-                Contact and Resume
+                Contacts
               </NavLink>
             </li>
             <button {...styles} onClick={changeTheme}>
