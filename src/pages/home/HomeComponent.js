0f7665dd2068f26527a19bdd006d1665fe1e40c3
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2024-10-22 20:35:04 +0700

    try to add visitors record

--- src/pages/home/HomeComponent.js
+++ src/pages/home/HomeComponent.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Greeting from "../../containers/greeting/Greeting";
 import Skills from "../../containers/skills/Skills";
 import Footer from "../../components/footer/Footer";
+import axios from "axios";
+
+const spreadsheetURL = process.env.REACT_APP_SHEET_URL;
+const API_KEY = process.env.REACT_APP_IPINFO_API_KEY;
 
 function Home(props) {
+  const [visitorData, setVisitorData] = useState(null);
+
+  useEffect(() => {
+    const fetchVisitorData = async () => {
+      try {
+        const response = await axios.get(`https://ipinfo.io?token=${API_KEY}`);
+        setVisitorData(response.data);
+
+        // Send the visitor data to the Google Apps Script web app
+        await axios.post(spreadsheetURL, response.data);
+        console.log("Visitor data saved to Google Sheets");
+      } catch (error) {
+        console.error("Error fetching or saving visitor data:", error);
+      }
+    };
+
+    fetchVisitorData();
+  }, []);
+  console.log(visitorData);
   return (
     <div>
       <Header theme={props.theme} setTheme={props.setTheme} />
