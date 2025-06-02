8ce84a4569c6964ea3f8654ded773d493a5ea3a4
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2025-06-02 13:41:25 +0700

    fix build

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { Fade } from "react-reveal";
 import { NavLink, Link } from "react-router-dom";
